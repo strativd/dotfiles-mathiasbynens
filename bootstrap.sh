c16eeabdcfccc2ac364f97efcac3ebde81debd76
Strat Barrett <strat.barrett@shopify.com>
2021-12-06 17:08:58 -0800

    Link from script directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,18 +4,20 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 function linkEmUp() {
-  ln .editorconfig ~/.editorconfig
-  ln .exports ~/.exports
-  ln .gitconfig ~/.gitconfig
-  ln .gvimrc ~/.gvimrc
-  ln .inputrc ~/.inputrc
-  ln .tmux.conf ~/.tmux.conf
-  ln .vimrc ~/.vimrc
-  ln .zshrc ~/.zshrc
-  ln .functions ~/.functions
-  ln .aliases ~/.aliases
-  ln .extra ~/.extra
+  ln -sf "$SCRIPT_DIR/.editorconfig" ~/.editorconfig
+  ln -sf "$SCRIPT_DIR/.exports" ~/.exports
+  ln -sf "$SCRIPT_DIR/.gitconfig" ~/.gitconfig
+  ln -sf "$SCRIPT_DIR/.gvimrc" ~/.gvimrc
+  ln -sf "$SCRIPT_DIR/.inputrc" ~/.inputrc
+  ln -sf "$SCRIPT_DIR/.tmux.conf" ~/.tmux.conf
+  ln -sf "$SCRIPT_DIR/.vimrc" ~/.vimrc
+  ln -sf "$SCRIPT_DIR/.zshrc" ~/.zshrc
+  ln -sf "$SCRIPT_DIR/.functions" ~/.functions
+  ln -sf "$SCRIPT_DIR/.aliases" ~/.aliases
+  ln -sf "$SCRIPT_DIR/.extra" ~/.extra
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
